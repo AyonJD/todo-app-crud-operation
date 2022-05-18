3cf9b23511e4a73ab81ce2c3a181e47d1240fcfe
Ayon <ayonjodder177@gmail.com>
2022-05-18 22:40:20 +0600

    Todo done

--- src/Components/ToDoApp/ToDoApp.js
+++ src/Components/ToDoApp/ToDoApp.js
@@ -1,10 +1,80 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const ToDoApp = () => {
+    const [messageList, setMessageList] = useState(["Milk", "Sugar", "Butter"]);
+
+    const addTodo = (message) => {
+        setMessageList([...messageList, message]);
+    };
+
+    const deleteTodo = (message) => {
+        let deleteMessageIndex = messageList.indexOf(message);
+        setMessageList([
+            ...messageList.slice(0, deleteMessageIndex),
+            ...messageList.slice(deleteMessageIndex + 1)
+        ]);
+    };
+
     return (
-        <div>
-            
+        <div id="app">
+            <div id="header">
+                <h2>Todo List</h2>
+            </div>
+            <TodoForm addTodo={addTodo} /> <br /> {/* Why */}
+            <TodoList messageList={messageList} deleteTodo={deleteTodo} />
         </div>
+    );
+};
+
+
+
+const TodoForm = ({ addTodo }) => {
+    const [input, setInput] = useState("");
+
+    const changeHandler = (event) => {
+        setInput(event.target.value);
+    };
+
+    const submitHandler = (event) => {
+        addTodo(input);
+        setInput("");
+    };
+
+    return (
+        <div id="form">
+            <input
+                id="form__input"
+                type="text"
+                value={input}
+                onChange={changeHandler}
+            />
+            <button id="form__submit" onClick={submitHandler}>
+                Add Todo
+            </button>
+        </div>
+    );
+};
+
+const TodoList = ({ messageList, deleteTodo }) => (
+    <ol id="todolist">
+        {messageList.map((message, index) => (
+            <Todo message={message} deleteTodo={deleteTodo} key={index} />
+        ))}
+    </ol>
+);
+
+const Todo = ({ message, deleteTodo }) => {
+    const handleSubmit = (event) => {
+        deleteTodo(message);
+    };
+
+    return (
+        <li id="todo">
+            <span id="todo__label">{message + "  "}</span>
+            <button id="todo__delete" onClick={handleSubmit}>
+                Delete
+            </button>
+        </li>
     );
 };
 
